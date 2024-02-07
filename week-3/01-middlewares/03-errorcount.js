2235ffbd7b7e719e0b2cb5464c23b31884ba2cb7
deepak25802 <deepakmani115@gmail.com>
2024-02-07 16:10:20 +0530

    Solved Week-3 middleware assignment

--- week-3/01-middlewares/03-errorcount.js
+++ week-3/01-middlewares/03-errorcount.js
@@ -9,6 +9,12 @@ let errorCount = 0;
 // Your task is to
 // 1. Ensure that if there is ever an exception, the end user sees a status code of 404
 // 2. Maintain the errorCount variable whose value should go up every time there is an exception in any endpoint
+const errorCountMiddleware = (err, req, res, next) => {
+  if(err) {
+    errorCount++;
+    res.status(404).send();
+  }
+}
 
 app.get('/user', function(req, res) {
   throw new Error("User not found");
@@ -22,5 +28,7 @@ app.post('/user', function(req, res) {
 app.get('/errorCount', function(req, res) {
   res.status(200).json({ errorCount });
 });
+
+app.use(errorCountMiddleware);
 
 module.exports = app;
